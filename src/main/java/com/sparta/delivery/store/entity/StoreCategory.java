54db7b7757119f0f27ab285103cb374ffb5cf087
oyeonjoo <oyj0842@naver.com>
2025-02-18 05:22:42 +0900

    Refactor: StoreCategory enum 문자열 매핑 메서드 추가

--- src/main/java/com/sparta/delivery/store/entity/StoreCategory.java
+++ src/main/java/com/sparta/delivery/store/entity/StoreCategory.java
@@ -2,7 +2,9 @@ package com.sparta.delivery.store.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+import com.sparta.delivery.aaglobal.GlobalException;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @JsonFormat(shape = Shape.OBJECT)
@@ -18,5 +20,14 @@ public enum StoreCategory {
 
   StoreCategory(String name) {
     this.name = name;
+  }
+
+  public static StoreCategory fromString(String category) {
+    for (StoreCategory storeCategory : values()) {
+      if (storeCategory.getName().equals(category)) {
+        return storeCategory;
+      }
+    }
+    throw new GlobalException(HttpStatus.BAD_REQUEST, "존재하지 않는 카테고리입니다.");
   }
 }
