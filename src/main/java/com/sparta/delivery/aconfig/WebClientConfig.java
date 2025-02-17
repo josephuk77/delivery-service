447909f1262393a1a02db643627927502298a002
jhwan <jhwan.kim95@gmail.com>
2025-02-17 15:48:47 +0900

    Refactor WebClientConfig.java를 aconfig로 이동

--- src/main/java/com/sparta/delivery/aconfig/WebClientConfig.java
+++ src/main/java/com/sparta/delivery/aconfig/WebClientConfig.java
@@ -1,4 +1,4 @@
-package com.sparta.delivery.aaglobal;
+package com.sparta.delivery.aconfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
