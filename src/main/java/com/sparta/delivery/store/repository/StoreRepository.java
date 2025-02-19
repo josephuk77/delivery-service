3378e519f75544b2758b142a9ba6978abe8cfbaf
oyeonjoo <oyj0842@naver.com>
2025-02-20 04:40:37 +0900

    Refactor: category 파라미터 바인딩

--- src/main/java/com/sparta/delivery/store/repository/StoreRepository.java
+++ src/main/java/com/sparta/delivery/store/repository/StoreRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -16,5 +17,5 @@ public interface StoreRepository extends JpaRepository<Store, UUID> {
   Page<Store> findAllByName(String keyword, Pageable pageable);
 
   @Query("SELECT s FROM Store s WHERE s.category = :category AND s.deletedAt IS NULL")
-  Page<Store> findAllByCategory(StoreCategory storeCategory, Pageable pageable);
+  Page<Store> findAllByCategory(@Param("category") StoreCategory storeCategory, Pageable pageable);
 }
