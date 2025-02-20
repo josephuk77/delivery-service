eeab2914d3def46f6cd1c5c47da571a3130cc9b6
oyeonjoo <oyj0842@naver.com>
2025-02-21 04:06:58 +0900

    Refactor: ReviewRequestDto에 validation 어노테이션 추가

--- src/main/java/com/sparta/delivery/review/dto/ReviewRequestDto.java
+++ src/main/java/com/sparta/delivery/review/dto/ReviewRequestDto.java
@@ -1,5 +1,7 @@
 package com.sparta.delivery.review.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,6 +14,11 @@ public class ReviewRequestDto {
 
   private Long userId;
   private UUID orderId;
+
+  @NotBlank
   private Integer star;
+
+  @NotBlank
+  @Size(min = 10)
   private String content;
 }
